a79f2f5575d2d1c683976158b60aba9a8a0e5f79
szymon <szymon.adp@gmail.com>
2022-09-05 20:02:33 +0200

    Szymon | pubspec.yaml clearance, ThemeData changes

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart'; niepotrzebne
 
 class ShoeData {
   final int id;
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:settings_ui/main.dart';
+import 'package:Drop_Guide/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
